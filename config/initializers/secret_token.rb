f25da2611283188320a83e708a621b9bcd378e5d
Matthew Williams <m.williams@me.com>
2014-04-07 20:16:38 +0100

    Remove environment variable check for security token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,6 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 # RevueIo::Application.config.secret_token = "crRKuXbqqDbEGMKDyFU8h_Ys9gXENHHoCfxi6PJu7hQ7xzc-UfZmW6OJ2vM460sThnFJAuZ_TM0TqHTs9HTllQ"
-class SecurityError < Exception; end
 RevueIo::Application.config.secret_token = ENV['revue_secret_token'] || "DEVELOPER_SECRET_TOKEN_DO_NOT_USE_IN_PRODUCTION"
-raise SecurityError("Found the developer secret token in the production environment") if Rails.env.production? and ENV['revue_secret_token'].nil?
